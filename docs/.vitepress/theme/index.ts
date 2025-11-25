7bb650321f7fa272683490c67d0731add0323614
Alberto Scandolara <alberto.scandolara92@gmail.com>
2025-11-25 22:56:43 +0100

    fix: fixed SSR rendering

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,11 +2,13 @@ import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 
 import Carousel from "../components/Carousel.vue";
+import FeatureCards from "../components/FeatureCards.vue";
 
 export default {
     extends: DefaultTheme,
     enhanceApp({ app }) {
         // register your custom global components
-        app.component('Carousel', Carousel)
+        app.component('Carousel', Carousel);
+        app.component('FeatureCards', FeatureCards);
     }
 } satisfies Theme
