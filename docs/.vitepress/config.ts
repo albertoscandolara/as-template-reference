e92238204783c075eb4e9b2f269560e3c018fb3e
Alberto Scandolara <alberto.scandolara92@gmail.com>
2025-12-07 21:52:02 +0100

    docs(sidebar): added new sections to sidebar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,7 +14,10 @@ export default defineConfig({
         items: [
           { text: 'Husky', link: '/pages/01-husky' },
           { text: 'Branchlint', link: '/pages/02-branchlint' },
-          { text: 'Commitlint', link: '/pages/03-commitlint' }
+          { text: 'Commitlint', link: '/pages/03-commitlint' },
+          { text: 'Documentation', link: '/pages/04-documentation' },
+          { text: 'Semantic release', link: '/pages/05-semantic-release' },
+          { text: 'Commitizen', link: '/pages/06-commitizen' }
         ]
       }
     ],
